88dffd9e599c0e52db81c8347e0136558a4ac920
Stas <stas_kriger@mail.ru>
2024-09-26 19:34:06 +0300

    Вынес логику событий в loginForm

--- source/pages/LoginPage/LoginPage.js
+++ source/pages/LoginPage/LoginPage.js
@@ -1,7 +1,5 @@
 import {LoginForm} from '../../widgets/loginForm/loginForm.js';
-import {sendLoginRequest} from './api/ajax.js';
-import { validateEmail } from '../../shared/validation/EmailValidation.js';
-import { validatePassword } from '../../shared/validation/PasswordValidation.js';
+
 
 
 export class RenderLogin {
@@ -11,68 +9,10 @@ export class RenderLogin {
     }
 
     render() {
-        const form = new LoginForm();
+        const form = new LoginForm(this.#parent);
         
-        const loginForm = form.renderTemplate();
-        this.#parent.innerHTML = loginForm;
-
-        const documentForm = this.#parent.querySelector('form');
-
-        const btn = this.#parent.querySelector('#Create');
-
-        documentForm.addEventListener('submit', (e) => {
-            e.preventDefault();
-            const textPass = this.#parent.querySelector('#errorPassword');
-            const textLogin = this.#parent.querySelector('#errorLogin');
-
-            textLogin.textContent = '';
-            textPass.textContent = '';
-
-            const loginInput = this.#parent.querySelector('#login');
-            loginInput.classList.remove('error');
-
-            const passwordInput = this.#parent.querySelector('#password');
-            passwordInput.classList.remove('error');
-            
-
-            const email = loginInput.value.trim();
-            const password = passwordInput.value;
-            if (!validateEmail(email)) { 
-                loginInput.classList.add('error');
-                textLogin.textContent = "Неверный логин";
-                return;
-            } 
-            if (!validatePassword(password)) {
-                passwordInput.classList.add('error');
-                textPass.textContent = "Неверный пароль";
-                return;
-            }
-
-            sendLoginRequest('POST', '/signin', {email, password}, (status, body) => {
-                if (status == 200) {
-                    this.#parent.removeChild(this.#parent.querySelector('#left'));
-                    this.#parent.removeChild(this.#parent.querySelector('#right'));
-                    sendLoginRequest('GET', '/auth', null, (status) => {
-                        if (status == 200) {
-                            alert('я тут');
-                        }
-                    })
-                    return
-                } else {
-                    const message = JSON.parse(body);
-                    loginInput.classList.add('error');
-                    passwordInput.classList.add('error');
-                    textLogin.textContent = message.error;
-                }
-                
-            });
-        })
-
-        btn.addEventListener('click', () => {
-            this.#parent.removeChild(this.#parent.querySelector('#left'));
-            this.#parent.removeChild(this.#parent.querySelector('#right'));
-            // будет рендер
-        })
+        form.renderTemplate();
+        
     }
     
 }
--- source/widgets/loginForm/loginForm.js
+++ source/widgets/loginForm/loginForm.js
@@ -1,16 +1,76 @@
-'use strict'
+import {sendLoginRequest} from '../../pages/LoginPage/api/ajax.js';
+import { validateEmail } from '../../shared/validation/EmailValidation.js';
+import { validatePassword } from '../../shared/validation/PasswordValidation.js';
 
 export class LoginForm {
     #parent
     constructor(parent) {
         this.#parent = parent
+        
     }
 
+    
 
     renderTemplate() {
-        const template = Handlebars.templates['loginForm.hbs'];  
-        
-        return template();
+        const template = Handlebars.templates['loginForm.hbs'];
+        this.#parent.innerHTML = template();  
+        this.#parent.querySelector('#Create').addEventListener('click', () => {
+            this.#parent.removeChild(this.#parent.querySelector('#left'));
+            this.#parent.removeChild(this.#parent.querySelector('#right'));
+            // будет рендер
+        })
+
+        const documentForm = this.#parent.querySelector('form');
+
+        const btn = this.#parent.querySelector('#Create');
+
+        documentForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const textPass = this.#parent.querySelector('#errorPassword');
+            const textLogin = this.#parent.querySelector('#errorLogin');
+
+            textLogin.textContent = '';
+            textPass.textContent = '';
+
+            const loginInput = this.#parent.querySelector('#login');
+            loginInput.classList.remove('error');
+
+            const passwordInput = this.#parent.querySelector('#password');
+            passwordInput.classList.remove('error');
+            
+
+            const email = loginInput.value.trim();
+            const password = passwordInput.value;
+            if (!validateEmail(email)) { 
+                loginInput.classList.add('error');
+                textLogin.textContent = "Неверный логин";
+                return;
+            } 
+            if (!validatePassword(password)) {
+                passwordInput.classList.add('error');
+                textPass.textContent = "Неверный пароль";
+                return;
+            }
+
+            sendLoginRequest('POST', '/signin', {email, password}, (status, body) => {
+                if (status == 200) {
+                    this.#parent.removeChild(this.#parent.querySelector('#left'));
+                    this.#parent.removeChild(this.#parent.querySelector('#right'));
+                    sendLoginRequest('GET', '/auth', null, (status) => {
+                        if (status == 200) {
+                            alert('я тут');
+                        }
+                    })
+                    return
+                } else {
+                    const message = JSON.parse(body);
+                    loginInput.classList.add('error');
+                    passwordInput.classList.add('error');
+                    textLogin.textContent = message.error;
+                }
+                
+            });
+        })
     }
 
 }
