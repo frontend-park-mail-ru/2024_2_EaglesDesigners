cc2c5efd33f61efcd6223e058efd1fc9208103ac
Stas <stas_kriger@mail.ru>
2024-09-29 01:32:07 +0300

    фикс сервер и название файлов

--- server/server.js
+++ server/server.js
@@ -1,7 +1,4 @@
-
-
 const body = require('body-parser');
-const uuid = require('uuid').v4;
 
 const express = require('express');
 
@@ -13,51 +10,9 @@ app.use(express.static(require('path').join(__dirname, '..', '/source')));
 app.use(express.static(require('path').resolve(__dirname, '..', 'node_modules')));
 app.use(body.json());
 
-const users = {
-    'staskriger72@gmail.com': {
-        email: 'staskriger72@gmail.com',
-        password: 'password',
-    },
-    'stas_kriger@mail.ru': {
-        email: 'stas_kriger@mail.ru',
-        password: 'password',
-    },
-    'aleksandr.tsvetkov@corp.mail.ru': {
-        email: 'aleksandr.tsvetkov@corp.mail.ru',
-        password: 'password',
-        age: 30,
-        images: [4, 5]
-    },
-    'a.ostapenko@corp.mail.ru': {
-        email: 'a.ostapenko@corp.mail.ru',
-        password: 'password',
-        age: 21
-    }
-};
 
-const ids = {};
-
-app.post('/login', (req, res) => {
-    const password = req.body.password;
-    const email = req.body.email;
-    if (!password || !email) {
-        return res.status(400).json({ error: 'Не указан E-Mail или пароль' });
-    }
-    if (!users[email] || users[email].password !== password) {
-        return res.status(400).json({ error: 'Не верный E-Mail и/или пароль' });
-    }
-
-    const id = uuid();
-    ids[id] = email;
-
-    res.cookie('podvorot', id, {
-        expires: new Date(Date.now() + 1000 * 60 * 10)
-    });
-    res.status(200).json({ message: "authentication successful" });
-});
 
 const port = 8001
-const hostname = '127.0.0.1'
 
 // Создаём HTTP-сервер
 app.get('/', (req, res) => {
@@ -65,24 +20,12 @@ app.get('/', (req, res) => {
   res.sendFile(require('path').join(__dirname, '..', '/source/index.html'))
 });
 
-app.get('/signup', (req, res) => {
-    return res.status(200).json({});
-})
 
-app.get('/auth', (req, res) => {
-    return res.status(404).json({
-       "error": "Unauthorized",
-       "status": "error"
-    })
-    return res.status(200).json({"user": {
-        "id": 0,
-        "username": "user2",
-        "name": "Жабка пепе"
-    }});
-})
+
+
 
 // Выводим лог как только сервер будет запущен
-app.listen(port, hostname, () => {
+app.listen(port, () => {
 
 });
 console.log("Server start");
