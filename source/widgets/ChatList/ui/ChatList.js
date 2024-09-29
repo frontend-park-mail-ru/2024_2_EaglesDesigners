cfa56016c3e6f42677dd0e6ed256869bea903848
Vladimir Svetlakov <vova.svetl@mail.ru>
2024-09-29 13:48:00 +0300

    Добавлены аватарки в списке чатов по url

--- source/widgets/ChatList/ui/ChatList.js
+++ source/widgets/ChatList/ui/ChatList.js
@@ -8,9 +8,9 @@ export class ChatList{
 
     render(){
         const chat = [
-        { id : "prop1", name : "Борат", lastMessage: "Купи мне деньга" },
-        { id : "prop2", name : "Егор Крид", lastMessage: "Мама мама мама круго голова" },
-        { id : "prop3", name : "Коллектор", lastMessage: "мы тебе дверь снесем слышиш выходи пока можешь" }
+        { id : "prop1", name : "Борат", avatarURL:"https://i.pinimg.com/originals/fa/9c/13/fa9c13a52830c584fa8b96dab8652023.jpg", lastMessage: "Купи мне деньга" },
+        { id : "prop2", name : "Егор Крид", avatarURL:"https://chpic.su/_data/stickers/a/Avatars_Emoji/Avatars_Emoji_003.webp",lastMessage: "Мама мама мама круго голова" },
+        { id : "prop3", name : "Коллектор", avatarURL:"https://chpic.su/_data/stickers/a/Avatars_Emoji/Avatars_Emoji_003.webp",lastMessage: "мы тебе дверь снесем слышиш выходи пока можешь" }
     ];
         const template = Handlebars.templates.ChatList;
         this.#parent.innerHTML = template({chat});   
