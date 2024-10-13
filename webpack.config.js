3a37232cd6c55ff1bf6b330c311f183ae1e78e6f
Vladimir Svetlakov <vova.svetl@mail.ru>
2024-10-13 19:40:50 +0300

    Исправил сборку .ts файлов через babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,15 @@ module.exports = {
   entry: './source/index.ts',
   module: {
     rules: [
-      { test: /\.(js|ts)$/, use: 'babel-loader'},
+      { test: /\.(js|ts)$/, use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+              ['@babel/preset-typescript', { allowNamespaces: true }]
+          ],
+        }
+      }]
+      },
       { test: /\.(scss|css)$/, use: [ 'style-loader', 'css-loader', 'sass-loader' ] },
       { test: /\.(handlebars|hbs)$/, use: 'handlebars-loader' },
     ]
