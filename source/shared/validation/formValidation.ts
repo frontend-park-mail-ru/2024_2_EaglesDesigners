9bb64f3e471800c35960323defaf4827ab3a1b22
Vladimir Svetlakov <vova.svetl@mail.ru>
2024-10-14 01:57:38 +0300

    Расписал типы в валидаторах

--- source/shared/validation/formValidation.ts
+++ source/shared/validation/formValidation.ts
@@ -3,7 +3,7 @@
  * @param {object, text, object} classObject - object where happened error, error - content of error, text - object where add text of error
  * @returns {}
  */
-export const validateForm = (classObject, error, text) => {
+export const validateForm = (classObject: HTMLInputElement, error: string, text: HTMLSpanElement) => {
   classObject.classList.add("error");
   text.textContent = error;
 };
