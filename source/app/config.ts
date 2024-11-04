bfaf54a6bbfd46b9e51f122d6d452918d3071367
Vladimir Svetlakov <vova.svetl@mail.ru>
2024-11-04 05:35:15 +0300

    Вынес ChatType как отдельный тип

--- source/app/config.ts
+++ source/app/config.ts
@@ -1,2 +1,8 @@
+export const ChatLabel = {
+    'personal': 'Личный чат',
+    'group': 'Группа',
+    'channel': 'Канал'
+};
+
 export const localHost = "http://localhost:8080";
 export const serverHost = "http://212.233.98.59:8080";
--- source/entities/Chat/model/type.ts
+++ source/entities/Chat/model/type.ts
@@ -1,10 +1,12 @@
 import { TChatMessage } from "@/entities/ChatMessage";
 
+export type ChatType = "personal" | "group" | "channel";
+
 export interface TChat {
   avatarURL: string;
   chatId: string;
   chatName: string;
-  chatType: "personalMessages" | "group" | "channel";
+  chatType: ChatType;
   lastMessage: TChatMessage;
   usersId: number[];
 }
