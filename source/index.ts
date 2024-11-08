bd40b1b7b283c35e0ed79c5a556f2d5254df2c1b
Stas <stas_kriger@mail.ru>
2024-11-08 04:42:28 +0300

    check

--- source/index.ts
+++ source/index.ts
@@ -1,7 +1,7 @@
 import { App } from "./app";
 import runtime from "serviceworker-webpack5-plugin/lib/runtime";
 
-console.log('asdad');
+console.log(navigator,'serviceWorker' in navigator );
 if ('serviceWorker' in navigator) {
     console.log("asdasdas");
    runtime.register();
