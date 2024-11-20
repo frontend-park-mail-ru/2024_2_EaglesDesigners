99f2bd516e9075406ab71208aab839f6d1794e4c
Vladimir Svetlakov <vova.svetl@mail.ru>
2024-11-20 22:22:10 +0300

    Поправил обработку сообщений по вебсовету

--- source/pages/MainPage/ui/handlers.ts
+++ source/pages/MainPage/ui/handlers.ts
@@ -1,8 +1,8 @@
 import { ChatStorage } from "@/entities/Chat/lib/ChatStore";
-import { TChatMessage } from "@/entities/ChatMessage";
 import { UserStorage } from "@/entities/User";
+import { TMessageWS } from "@/shared/api/types";
 
-export const renderMessage = async (message: TChatMessage) => {
+export const renderMessage = async (message: TMessageWS) => {
   if (message.chatId !== ChatStorage.getChat().chatId) {
     return;
   }
