1f2dbcafa888bdfd95456bcedc900ea257d82982
Stas <stas_kriger@mail.ru>
2024-12-01 14:46:25 +0300

    Добавил конфиг для nginx

--- source/app/config.ts
+++ source/app/config.ts
@@ -5,6 +5,6 @@ export const ChatLabel = {
 };
 
 //export const serverHost = "http://localhost:8080";
-export const serverHost = "http://212.233.98.59:8080";
+export const serverHost = "http://212.233.98.59:8080/api";
 //export const websocketHost = "http://localhost:8083";
-export const websocketHost = "http://212.233.98.59:8083";
+export const websocketHost = "http://212.233.98.59:8083/api";
