9e49d8c7c7943d1a37107ff07e5f468ee0e459f1
Stas <stas_kriger@mail.ru>
2024-12-01 17:38:39 +0300

    поменял урл до вебсокета

--- source/app/config.ts
+++ source/app/config.ts
@@ -6,5 +6,4 @@ export const ChatLabel = {
 
 //export const serverHost = "http://localhost:8080";
 export const serverHost = "https://patefon.site/api";
-//export const websocketHost = "http://localhost:8083";
-export const websocketHost = "https://patefon.site/api";
+export const websocketHost = "wss://patefon.site:8083/api";
